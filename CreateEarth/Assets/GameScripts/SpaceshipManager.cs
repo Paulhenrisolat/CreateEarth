a905112a3a833ea2aa55039a65d2290871603316
Paulhenrisolat <paulhenrisolat@gmail.com>
2023-05-28 22:11:14 +0200

    Beta Build earth Update

--- CreateEarth/Assets/GameScripts/SpaceshipManager.cs
+++ CreateEarth/Assets/GameScripts/SpaceshipManager.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class SpaceshipManager : MonoBehaviour
 {
+    private GameObject earthtest;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        earthtest = GameObject.Find("EarthTest");
+        earthtest.SetActive(false);
     }
 
     // Update is called once per frame
@@ -21,6 +24,7 @@ public class SpaceshipManager : MonoBehaviour
         if (ComputerToSpaceShip.canCreateEarth == true)
         {
             print("earth is here");
+            earthtest.SetActive(true);
             ComputerToSpaceShip.canCreateEarth = false;
         }
     }
